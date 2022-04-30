cf00045851007963d3ce65270d33895f16502f51
MbRenan <renan.nunes@mblabs.com.br>
2022-04-30 17:49:34 -0300

    test: ensure SignUpController returns 400 if no email is provided

--- src/presentation/controller/signup/signup-controller.spec.ts
+++ src/presentation/controller/signup/signup-controller.spec.ts
@@ -1,6 +1,21 @@
+import { SignUpController } from './signup-controller'
+
+interface ISutTypes {
+  sut: SignUpController
+}
+
+const makeSut = (): ISutTypes => {
+  const sut = new SignUpController()
+  return {
+    sut
+  }
+}
+
 describe('SignUp Controller', () => {
-  test('shloud return 1 +1 true', () => {
-    const result = 1 + 1
-    expect(result).toBe(2)
+  test('shloud return 1 +1 true', async () => {
+    const { sut } = makeSut()
+    const response = await sut.handle()
+    expect(response.status).toBe(400)
+    expect(response.body).toEqual(new Error('missing email'))
   })
 })
