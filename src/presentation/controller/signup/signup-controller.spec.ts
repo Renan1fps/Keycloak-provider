3dd1e5a9907e864220952e1ee3133e050dcdf16c
MbRenan <renan.sergipe68@gmail.com>
2022-04-30 20:25:18 -0300

    test: ensure SignUpController calls addAccountUseCase with correct values

--- src/presentation/controller/signup/signup-controller.spec.ts
+++ src/presentation/controller/signup/signup-controller.spec.ts
@@ -1,3 +1,5 @@
+import { IAccountModel } from '../../../domain/models/account'
+import { IAddAccountModel, IAddAccountUseCase } from '../../../domain/usecase/add-account'
 import { SignUpController } from './signup-controller'
 import {
   IEmailValidator,
@@ -5,6 +7,19 @@ import {
   MissingParam,
   ServerError
 } from './signup-protocols'
+
+const makeAddAccountUseCase = (): IAddAccountUseCase => {
+  class AddAccountUseCaseStup implements IAddAccountUseCase {
+    async add (account: IAddAccountModel): Promise<IAccountModel> {
+      const fakeAccount = {
+        idKeycloak: 'valid_id'
+      }
+      return await new Promise(resolve => resolve(fakeAccount))
+    }
+  }
+
+  return new AddAccountUseCaseStup()
+}
 
 const makeEmailValidator = (): IEmailValidator => {
   class EmailValidatorStub implements IEmailValidator {
@@ -18,14 +33,17 @@ const makeEmailValidator = (): IEmailValidator => {
 interface ISutTypes {
   sut: SignUpController
   mailValidator: IEmailValidator
+  addAccountUseCase: IAddAccountUseCase
 }
 
 const makeSut = (): ISutTypes => {
   const mailValidator = makeEmailValidator()
-  const sut = new SignUpController(mailValidator)
+  const addAccountUseCase = makeAddAccountUseCase()
+  const sut = new SignUpController(mailValidator, addAccountUseCase)
   return {
     sut,
-    mailValidator
+    mailValidator,
+    addAccountUseCase
   }
 }
 
@@ -130,5 +148,22 @@ describe('SignUp Controller', () => {
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toEqual(new ServerError())
+  })
+
+  test('Should calls addAccountUseCase with corret values', async () => {
+    const { sut, addAccountUseCase } = makeSut()
+    const isValidSpy = jest.spyOn(addAccountUseCase, 'add')
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    await sut.handle(httpRequest)
+    expect(isValidSpy).toHaveBeenCalledWith({
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
   })
 })
